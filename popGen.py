60893ef02544641c9114c3a340427bee67886cd3
Joshua Graham <jsgraham2001@gmail.com>
2022-09-02 21:05:23 -0500

    Class File and Server

---
+++ popGen.py
@@ -0,0 +1,102 @@
+from collections import Counter
+import numpy as np
+
+class PopGen:
+
+    def __init__(self, N, MALE_PROPORTION):
+        self.N = N
+        self.MALE_PROPORTION = MALE_PROPORTION
+
+    def getN(self):
+        return self.N
+
+    def getMALE_PROPORTION(self):
+        return self.MALE_PROPORTION
+
+    #def make_couples(self, generation:np.ndarray, male_proportion:float = getMALE_PROPORTION) -> np.ndarray:
+    def make_couples(self, generation:np.ndarray) -> np.ndarray:
+        """Split the generation into male and female segments, form couples
+        
+        Returns: array of couple, where each couple is represented as the union of their ancestors
+        """
+        # population size and male_proportion are parameters to allow for more dynamic simulations
+        male_proportion = self.getMALE_PROPORTION()
+        population_size = len(generation)
+        num_males = int(male_proportion * population_size)
+        num_females = population_size - num_males
+        men = generation[:num_males]
+        women = generation[num_males:]
+        np.random.shuffle(men)
+        np.random.shuffle(women)
+        couples = []
+        for i in range(min(num_males, num_females)):
+            couples.append(men[i].union(women[i]))
+        if not num_males == num_females:
+            for i in range(abs(num_females - num_males)):
+                if num_males < num_females:
+                    couples.append(men[i].union(women[i + num_males]))
+                else:
+                    couples.append(women[i].union(men[i + num_females]))
+        return np.array(couples)
+
+    def create_next_generation(self, couples:np.ndarray) -> np.ndarray:
+        """Create the next population generation"""
+        population_size = self.getN()
+        num_couples = len(couples)
+        children_index = np.random.randint(0, high=num_couples, size=population_size)
+        return np.take(couples, children_index)
+
+    def count_descendants(self, generation:np.ndarray) -> Counter:
+        """Return a Counter with info on how many descendants first-gen ancestors have.
+        
+        Note: Ancestors with no descendants do not have an indexed entry in the Counter
+        """
+        c = Counter()
+        for individual in generation:
+            c += Counter(individual)
+        return c
+
+    def is_genealogical_ae_present(self, descendant_counts:Counter) -> bool:
+        population_size = self.getN()
+        if max(descendant_counts.values()) == population_size:
+            return True
+        else:
+            return False
+
+    def run_simulation(self):
+        generation = np.array([{i} for i in range(self.N)])
+        for i in range(1000):
+            couples = self.make_couples(generation)
+            next_gen = self.create_next_generation(couples)
+            descendants = self.count_descendants(next_gen)
+            if self.is_genealogical_ae_present(descendants):
+                break
+            else:
+                generation = next_gen
+
+        start_population:int
+        num_generations:int
+        num_ancestors_represented:int
+        num_ae_couples:int
+        distribution_of_couples_descendants:Counter
+
+        start_population = self.getN()
+        num_generations = i + 1 # Python iteration starts at zero, but we start at 1
+        num_ancestors_represented = len(descendants)
+        d = Counter(descendants.values())
+        c = Counter()
+        for k in d:
+            c[k] = int(d[k]/2)
+        distribution_of_couples_descendants = c
+        num_ae_couples = distribution_of_couples_descendants[self.getN()]
+
+        print("Initial ancestral couples:", int(start_population / 2))
+        print("Number of generations until A/E couple emerged:", num_generations)
+        print("Number of ancestral couples still in gene pool at simulation end:", int(num_ancestors_represented/2))
+        print("Number of A/E couples at simulation end:", int(num_ae_couples))
+        almost_ae = 0
+        for k in distribution_of_couples_descendants:
+            if k >= self.getN() * 0.9:
+                almost_ae += distribution_of_couples_descendants[k]
+        print("Number of ancestral couples who are ancestors of 90+% of final generation:", almost_ae)
+
---
+++ server.py
@@ -0,0 +1,88 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import popGen as p
+import json
+
+# Example URL
+# http://localhost:8080/popgen?10_000&0.5
+
+# Config
+
+configFileRaw = open("PopGen/config.json")
+configFile = json.load(configFileRaw)
+
+hostName = configFile["hostName"]
+serverPort = configFile["port"]
+
+#Logging
+
+path = configFile["logPath"]
+logging.basicConfig(filename=path,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    encoding="utf-8",
+    level=logging.DEBUG)
+
+#Server
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        logging.info(self.path)
+        logging.info(self.client_address)
+        logging.info(self.request_version)
+        path = self.path
+        if "?" in path:
+            tokens = path.split("?")
+            if len(tokens) == 2:
+                logging.info("Token 1: " + str(tokens[0]))
+                logging.info("Token 2: " + str(tokens[1]))
+            if tokens[0] == "/popgen":
+                try:
+                    args = tokens[1].split("&")
+                    N = int(args[0])
+                    MALE_PROPORTION = float(args[1])
+                    logging.info("N: " +  str(N) + "\nMALE_PROPORTION:" + str(MALE_PROPORTION))
+                except Exception as e:
+                    print("An exception occured.")
+                    logging.info(e)
+
+        if self.path.startswith('/popgen'):
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            try:
+                model = p.PopGen(N, MALE_PROPORTION)
+                model.run_simulation()
+                print("Simulation completed succesfully.")
+                self.wfile.write(bytes("Simulation completed succesfully.", "utf-8"))
+            except Exception as e:
+                print("An exception occured.")
+                logging.info(e)
+                self.wfile.write(bytes("An exception occured.", "utf-8"))
+            
+        elif self.path.endswith('/'):
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes("Population Geneology homepage.", "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes("404 Page Not Found", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("PopGen server started http://%s:%s" % (hostName, serverPort))
+    logging.info("PopGen server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
